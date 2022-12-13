b1661fe312b6cd4e697234dffe6005e5219a2da4
harveyrutland <hcrutland@mail.com>
2022-12-13 13:33:53 +0000

    push to sql

---
+++ pushtosql.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+
+data = pd.read_csv (r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')   
+df = pd.DataFrame(data)
+
+
+
+import sqlite3
+from google.cloud import storage
+import pandas as pd 
+import numpy as np
+from datetime import datetime
+import mysql.connector 
+import sys 
+import os
+
+
+cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='cabinet_datasets')
+cursor = cnx.cursor()
+cols = "`,`".join([str(i) for i in df.columns.tolist()])
+
+
+for i,row in df.iterrows():
+    sql = "INSERT INTO `current_data` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+    cursor.execute(sql, tuple(row))
+    cnx.commit()
+
+print(df) 
+
+
+
+# Create cursor
+my_cursor = cnx.cursor()
+
+
+cnx.close()
+# os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
