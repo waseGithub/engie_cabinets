6d4898f050541502cec02ce07cdac86ea1ccb674
harveyrutland <hcrutland@mail.com>
2022-12-14 10:49:21 +0000

    temperature development

---
+++ temperature.py
@@ -0,0 +1,55 @@
+import serial
+from serial import SerialException
+import time
+import csv
+import os
+import pandas as pd
+import numpy
+
+
+import serial.tools.list_ports
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+gauth = GoogleAuth()           
+drive = GoogleDrive(gauth)  
+
+import re
+import subprocess
+import pandas as pd 
+import dictpy
+
+device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+df = subprocess.check_output("lsusb")
+devices = []
+
+line1 = None
+line2 = None
+line3 = None 
+line4 = None 
+line5 = None
+
+
+
+
+     
+       
+
+
+ports = serial.tools.list_ports.comports(include_links =False)
+ls = []
+for port in ports:
+    print(port.device)
+    ls.append(port.device)
+   
+print(ls)
+
+
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+
+Megas = []
+unos = []
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
