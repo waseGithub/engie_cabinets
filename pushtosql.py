cabf6353e6cb40330cefd9be298b87c31666de98
harveyrutland <hcrutland@mail.com>
2022-12-15 20:25:28 +0000

    cancat embedism

--- pushtosql.py
+++ pushtosql.py
@@ -4,9 +4,20 @@
 
 import pandas as pd
 import numpy as np
+import sqlite3
+from google.cloud import storage
+import pandas as pd 
+import numpy as np
+from datetime import datetime
+import mysql.connector 
+import sys 
+import os
 
-data = pd.read_csv (r'/home/pi/wase-cabinet/file_7.csv')   
+# data = pd.read_csv (r'/home/pi/wase-cabinet/file_7.csv')   
+data = pd.read_csv(r'/home/harvey/Documents/WASE/wase-cabinet/file_7.csv') 
+data2 = pd.read_csv(r'/home/harvey/Documents/WASE/wase-cabinet/file_6.csv') 
 df = pd.DataFrame(data)
+df2 = pd.DataFrame(data)
 
 ls = ['datetime', 'error_status', 'column_c']
 
@@ -22,24 +33,65 @@ for i in np.arange(1,10):
 
 
 df.columns = ls
+df2.columns = ls
+
 df['datetime'] = pd.to_datetime(df['datetime'])
 df['datetime'] = df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+df2['datetime'] = pd.to_datetime(df2['datetime'])
+df2['datetime'] = df2['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+
 df = df.dropna()
+df2 = df2.dropna()
+
+df.set_index(['datetime', 'error_status', 'column_c'], inplace =True)
+df2.set_index(['datetime', 'error_status', 'column_c'], inplace =True)
+
+print(df2)
+
+cols = len(df.columns)
+cols_ls = np.linspace(0, cols, 10)
+print(cols_ls)
+
+cols_ls_len = len(cols_ls)
+index = 0 
+index2 = 9
+concat_df = pd.DataFrame()
+
+def rename_slice(d, col_name, dex_num):
+    d = d.rename({col_name + '_' + str(dex_num) : col_name}, axis='columns')
+    return d
 
 
 
+for i in np.arange(0, cols_ls_len-1, 1): 
+    index += 1
+    df_slice = df.iloc[:, int(cols_ls[int(i)]):int(cols_ls[int(i)+1])]
+    df_slice = rename_slice(df_slice, 'card_present_channel', index)
+    df_slice = rename_slice(df_slice, 'card_enabled_channel', index)
+    df_slice = rename_slice(df_slice, 'card_voltage_channel', index)
+    df_slice = rename_slice(df_slice, 'card_current_channel', index)
+    df_slice['tank_id'] = index
+    index2 += 1
+    
+    df_slice2 = df2.iloc[:, int(cols_ls[int(i)]):int(cols_ls[int(i)+1])]
+    df_slice2 = rename_slice(df_slice2, 'card_present_channel', index)
+    df_slice2 = rename_slice(df_slice2, 'card_enabled_channel', index)
+    df_slice2 = rename_slice(df_slice2, 'card_voltage_channel', index)
+    df_slice2 = rename_slice(df_slice2, 'card_current_channel', index)
+    df_slice2['tank_id'] = index2
+
+    concat_df = concat_df.append(df_slice)
+    concat_df = concat_df.append(df_slice2)
 
 
+concat_df.reset_index(inplace=True)
+concat_df.set_index(['datetime','tank_id', 'error_status', 'column_c'], inplace=True)
 
+concat_df.to_csv('out.csv')
+df = concat_df.reset_index()
 
-import sqlite3
-from google.cloud import storage
-import pandas as pd 
-import numpy as np
-from datetime import datetime
-import mysql.connector 
-import sys 
-import os
 
 
 cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='cabinet_datasets')
@@ -71,4 +123,6 @@ my_cursor = cnx.cursor()
 #     print(i)
 
 cnx.close()
-# os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
+
+
+os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
