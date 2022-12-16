a0b623e579771c8d6e648ff767554e4012418a4d
harveyrutland <hcrutland@mail.com>
2022-12-16 14:39:42 +0000

    resampled dataset to 5min

--- pushtosql.py
+++ pushtosql.py
@@ -13,11 +13,45 @@ import mysql.connector
 import sys 
 import os
 
+
+######################
+######################
+######################
+
+def resample_mean(df, time, cols, round_val):
+  df = df.groupby([pd.Grouper(freq=time, level='datetime'), pd.Grouper(level='tank_id'),pd.Grouper(level='error_status')])[cols].mean(numeric_only=True) 
+  df = df.round(round_val)
+  return df
+
+def resample_sum(df, time, cols, round_val):
+  df= df[(df >= 0.0).all(1)]
+  df = df.groupby([pd.Grouper(freq=time, level='datetime'), pd.Grouper(level='tank_id')])[cols].sum()
+  df = df.round(round_val)
+  return df
+
+def resample_max(df, time, cols, round_val):
+  df= df[(df >= 0.0).all(1)]
+  df = df.groupby([pd.Grouper(freq=time, level='datetime'), pd.Grouper(level='tank_id')])[cols].max()
+  df = df.round(round_val)
+  return df
+
+######################
+######################
+######################
+
 # data = pd.read_csv (r'/home/pi/wase-cabinet/file_7.csv')   
-data = pd.read_csv(r'/home/harvey/Documents/WASE/wase-cabinet/file_7.csv') 
-data2 = pd.read_csv(r'/home/harvey/Documents/WASE/wase-cabinet/file_6.csv') 
+data = pd.read_csv(r'/home/farscopestudent/Documents/WASE/wase-cabinet/file_7.csv') 
+data2 = pd.read_csv(r'/home/farscopestudent/Documents/WASE/wase-cabinet/file_6.csv') 
 df = pd.DataFrame(data)
 df2 = pd.DataFrame(data)
+
+
+
+######################
+######################
+######################
+
+
 
 ls = ['datetime', 'error_status', 'column_c']
 
@@ -35,24 +69,24 @@ for i in np.arange(1,10):
 df.columns = ls
 df2.columns = ls
 
-df['datetime'] = pd.to_datetime(df['datetime'])
-df['datetime'] = df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
-df2['datetime'] = pd.to_datetime(df2['datetime'])
-df2['datetime'] = df2['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
 
 
 
 df = df.dropna()
 df2 = df2.dropna()
 
+
+
+
 df.set_index(['datetime', 'error_status', 'column_c'], inplace =True)
 df2.set_index(['datetime', 'error_status', 'column_c'], inplace =True)
 
-print(df2)
+
 
 cols = len(df.columns)
 cols_ls = np.linspace(0, cols, 10)
-print(cols_ls)
+
 
 cols_ls_len = len(cols_ls)
 index = 0 
@@ -65,6 +99,8 @@ def rename_slice(d, col_name, dex_num):
 
 
 
+df_dict = {}
+
 for i in np.arange(0, cols_ls_len-1, 1): 
     index += 1
     df_slice = df.iloc[:, int(cols_ls[int(i)]):int(cols_ls[int(i)+1])]
@@ -72,39 +108,77 @@ for i in np.arange(0, cols_ls_len-1, 1):
     df_slice = rename_slice(df_slice, 'card_enabled_channel', index)
     df_slice = rename_slice(df_slice, 'card_voltage_channel', index)
     df_slice = rename_slice(df_slice, 'card_current_channel', index)
-    df_slice['tank_id'] = index
+
+
     index2 += 1
-    
     df_slice2 = df2.iloc[:, int(cols_ls[int(i)]):int(cols_ls[int(i)+1])]
     df_slice2 = rename_slice(df_slice2, 'card_present_channel', index)
     df_slice2 = rename_slice(df_slice2, 'card_enabled_channel', index)
     df_slice2 = rename_slice(df_slice2, 'card_voltage_channel', index)
     df_slice2 = rename_slice(df_slice2, 'card_current_channel', index)
-    df_slice2['tank_id'] = index2
 
-    concat_df = concat_df.append(df_slice)
-    concat_df = concat_df.append(df_slice2)
+    df_name =  str(index)
+    df_dict[df_name] = df_slice 
+
+    df_name =  str(index2)
+    df_dict[df_name] = df_slice2
+
+    
 
 
-concat_df.reset_index(inplace=True)
-concat_df.set_index(['datetime','tank_id', 'error_status', 'column_c'], inplace=True)
 
+concat_df = pd.concat(df_dict)
+concat_df.reset_index(inplace =True)
+
+concat_df = concat_df.loc[concat_df['card_present_channel'] == True]
+
+
+
+
+concat_df = concat_df.rename(columns={'level_0':'tank_id'})
+
+
+
+
+
+concat_df['datetime'] = pd.to_datetime(concat_df['datetime'])
+
+
+
+
+
+
+
+######### resample current data 5min #########
+
+
+concat_df.set_index(['datetime','tank_id','error_status'], inplace=True)
 concat_df.to_csv('out.csv')
-df = concat_df.reset_index()
+concat_df = resample_mean(concat_df, '5T', concat_df.columns, 3)
+concat_df.drop(concat_df.columns[[0]],axis=1,inplace=True)
+print(concat_df)
 
+
+concat_df = concat_df.reset_index()
+
+
+
+
+
+# df.drop(columns=['card_present_channel', 'card_enabled_channel'], inplace = True)
+concat_df['datetime'] = concat_df['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
 
 cnx = mysql.connector.connect(user='root', password='wase2022', host='34.89.81.147', database='cabinet_datasets')
 cursor = cnx.cursor()
-cols = "`,`".join([str(i) for i in df.columns.tolist()])
+cols = "`,`".join([str(i) for i in concat_df.columns.tolist()])
 
 
-for i,row in df.iterrows():
+for i,row in concat_df.iterrows():
     sql = "INSERT INTO `current_data` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
     cursor.execute(sql, tuple(row))
     cnx.commit()
 
-print(df) 
 
 
 
@@ -125,4 +199,4 @@ my_cursor = cnx.cursor()
 cnx.close()
 
 
-os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
+# os.remove(r'/home/wase-cabinet/wase-cabinet/flowmeter_push.csv')
