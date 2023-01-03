54a3395c5f19d47e6cb349756ed9cbfd6d3b75fa
harveyrutland <hcrutland@mail.com>
2023-01-03 16:57:40 +0000

    display_setup

--- SQLConnect.py
+++ SQLConnect.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
+
+
+
 
 #connect to mysql database
 naturedb = mysql.connector.connect(
@@ -20,12 +24,13 @@ flow_db = pd.read_sql('SELECT*FROM flowmeter', con=naturedb)
 
 #find unique reactors, then split their data into different dataframes
 reactor_names = flow_db['Name'].unique().tolist()
-flow_dict = {reactor: flow_db.loc[flow_db['Name'] == reactor] for reactor in reactor_names}
+print(reactor_names)
+
 
 #create line thickness dictionary, then make AD bolder
 thick_dict = {reactor: 2 for reactor in reactor_names}
 thick_dict.update({'4A':5, '4B':5, '4C':5})
-print(thick_dict)
+# print(thick_dict)
 
 #create colours dictionary for plot lines
 colours_dict = {'1A': '#012969', '1B': '#2358ad', '1C': '#5f95ed', 
@@ -34,35 +39,87 @@ colours_dict = {'1A': '#012969', '1B': '#2358ad', '1C': '#5f95ed',
                 '4A': '#6e036e', '4B': '#ba25ba', '4C': '#e629b6'
 }
 
-#voltage dictionary to differentiate between different voltages
-
-# #create plots of each different reactor
-plt.figure()
-for reactor in flow_dict:
-  sns.lineplot(x='index', y='total_vol_since_startml', data=flow_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
+print(flow_db)
 
 
 #read current data from sql to pandas
 current_db = pd.read_sql('SELECT*FROM current_data', con=naturedb)
+current_db['card_current_channel'] = current_db['card_current_channel'].astype('float')
 print(current_db)
 
-#find unique reactors, then split their data into different dataframes
-current_dict = {reactor: current_db.loc[current_db['Name'] == reactor] for reactor in reactor_names}
-#print(current_dict)
 
-#subsample each databse in curernt_dict as its quite big
-for reactor in current_dict:
-    current_dict[reactor] = current_dict[reactor].reset_index()
-    current_dict[reactor] = current_dict[reactor][::10]
-    current_dict[reactor]['card_current_channel'] = current_dict[reactor]['card_current_channel'].astype('float')
+
+
+
+
+df = current_db
+
+
+y_axis = 'Module Current'
+fig = go.Figure()
+  # df[column] = df[column][(df[[column]] < 30000).all(1)]
+  #  df[column] = df[column][(df[[column]] > 0).all(1)]
+  
+try:
+  df.reset_index(inplace = True)
+except ValueError:
+  pass
+
+for i in reactor_names:
+  print(i)
+  fig.add_trace(go.Scatter(
+          x=df[df['Name'] == i]['datetime'],
+          y=df[df['Name'] == i]['card_current_channel'],
+          name = 'Tank' + str(i),
+          line_shape='spline',
+          connectgaps= False,
+          line=dict(color= colours_dict.get(i))
+      # Style name/legend entry with html tags
+      ))
+
+fig.update_yaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
+fig.update_xaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
+fig.update_layout(legend=dict(orientation="v",yanchor="top",y=0.99,xanchor="right",x=1.01))
+fig.update_layout(template = "plotly_white", width=1000, height=600, yaxis_title=y_axis,font=dict(size=15), margin=dict(l=50,r=50,b=50,t=50,pad=3))
+
+fig.show()
+
+
+
+
+
+
+
+
+
+
+#voltage dictionary to differentiate between different voltages
+#flow_dict = {reactor: flow_db.loc[flow_db['Name'] == reactor] for reactor in reactor_names}
+
+# # #create plots of each different reactor
+# plt.figure()
+# for reactor in flow_dict:
+#   sns.lineplot(x='index', y='total_vol_since_startml', data=flow_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
+
+
+
+# #find unique reactors, then split their data into different dataframes
+# current_dict = {reactor: current_db.loc[current_db['Name'] == reactor] for reactor in reactor_names}
+# #print(current_dict)
+
+# #subsample each databse in curernt_dict as its quite big
+# for reactor in current_dict:
+#     current_dict[reactor] = current_dict[reactor].reset_index()
+#     current_dict[reactor] = current_dict[reactor][::10]
+#     current_dict[reactor]['card_current_channel'] = current_dict[reactor]['card_current_channel'].astype('float')
     
 
-print('test reactor', current_dict['1A']['card_current_channel'])
-#create plots of each different reactor
-plt.figure()
-for reactor in current_dict:
-  sns.lineplot(x='index', y='card_current_channel', data=current_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
+# print('test reactor', current_dict['1A']['card_current_channel'])
+# #create plots of each different reactor
+# # plt.figure()
+# # for reactor in current_dict:
+# #   sns.lineplot(x='index', y='card_current_channel', data=current_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
 
 
-plt.ylim(-5, 30)
-plt.show()
+# # plt.ylim(-5, 30)
+# # plt.show()
