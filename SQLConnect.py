a562106ff7e11dae6cc1c2803d97de029b24a0a1
harveyrutland <hcrutland@mail.com>
2023-01-04 17:04:55 +0000

    dashboard

--- SQLConnect.py
+++ SQLConnect.py
@@ -5,9 +5,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
 
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+from plotly.subplots import make_subplots
 
-def plot_plotly():
-  y_axis = 'Module Current'
+
+app = dash.Dash()
+
+
+def plot_plotly(df, y_axis, column, show_dash, index_ls):
+  
   fig = go.Figure()
     # df[column] = df[column][(df[[column]] < 30000).all(1)]
     #  df[column] = df[column][(df[[column]] > 0).all(1)]
@@ -16,28 +25,133 @@ def plot_plotly():
     df.reset_index(inplace = True)
   except ValueError:
     pass
+  
 
-  for i in reactor_names:
+  for i in index_ls:
     print(i)
+    if show_dash == True:
+      line_dict = {'1': '5px', 
+                  '2': '10px',
+                  '3': '1px',
+                  '4': '0px'}
+    else:
+      line_dict = {'1': '0px', 
+                  '2': '0px',
+                  '3': '0px',
+                  '4': '0px'} 
+      
+    line_width = {'1': 2, 
+                 '2': 2,
+                 '3': 2,
+                 '4': 2}
+    
     fig.add_trace(go.Scatter(
             x=df[df['Name'] == i]['datetime'],
-            y=df[df['Name'] == i]['card_current_channel'],
+            y=df[df['Name'] == i][column],
             name = 'Tank' + str(i),
             line_shape='spline',
-            connectgaps= False,
-            line=dict(color= colours_dict.get(i))
+            connectgaps= False, 
+            line=dict(color= colours_dict.get(i), width=line_width.get(i[0]), dash=line_dict.get(i[0]))
         # Style name/legend entry with html tags
         ))
 
   fig.update_yaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
   fig.update_xaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
   fig.update_layout(legend=dict(orientation="v",yanchor="top",y=0.99,xanchor="right",x=1.01))
-  fig.update_layout(template = "plotly_white", width=1000, height=600, yaxis_title=y_axis,font=dict(size=15), margin=dict(l=50,r=50,b=50,t=50,pad=3))
+  fig.update_layout(template = "plotly_white", height=900, yaxis_title=y_axis,font=dict(size=15), margin=dict(l=50,r=50,b=50,t=50,pad=3))
 
-  fig.show()
+ 
+  return fig 
 
 
 
+
+
+def plot_plotly_subplot(df, y_axis, column, index_ls, grph_num):
+  if grph_num > 3:
+    fig = make_subplots(rows=2, cols=2, subplot_titles=("0.8V Tank", "1V Tank", "1.3V Tank", 'AD Tank'))
+  else:
+    fig = make_subplots(rows=2, cols=2, subplot_titles=("0.8V Tank", "1V Tank", "1.3V Tank"))
+  X = range(1, 50+1)
+  X = list(X)
+  # colours = ['#595457','#9E1946', '#4D6CFA', '#DE0D92', '#C17817', '#4464AD', '#F8F991', '#2F0147']
+  
+  for i in index_ls[0:3]:
+    fig.add_trace(go.Scatter(
+              x=df[df['Name'] == i]['datetime'],
+              y=df[df['Name'] == i][column],
+              name = 'Tank' + str(i),
+              line_shape='spline',
+              connectgaps= False, 
+              line=dict(color= colours_dict.get(i), width=2)),row=1, col=1)
+    fig.update_yaxes(title_text="tank_1" + y_axis, row=1, col=1)
+  
+  for i in index_ls[3:6]:
+    fig.add_trace(go.Scatter(
+              x=df[df['Name'] == i]['datetime'],
+              y=df[df['Name'] == i][column],
+              name = 'Tank' + str(i),
+              line_shape='spline',
+              connectgaps= False, 
+              line=dict(color= colours_dict.get(i), width=2)),row=1, col=2)
+
+  
+  for i in index_ls[6:9]:
+    fig.add_trace(go.Scatter(
+              x=df[df['Name'] == i]['datetime'],
+              y=df[df['Name'] == i][column],
+              name = 'Tank' + str(i),
+              line_shape='spline',
+              connectgaps= False, 
+              line=dict(color= colours_dict.get(i), width=2)),row=2, col=1)
+    fig.update_yaxes(title_text= y_axis, row=2, col=1)
+    fig.update_xaxes(title_text= 'Datetime', row=2, col=1)
+  
+  if grph_num >  3:
+    for i in index_ls[9:12]:
+      fig.add_trace(go.Scatter(
+                x=df[df['Name'] == i]['datetime'],
+                y=df[df['Name'] == i][column],
+                name = 'Tank' + str(i),
+                line_shape='spline',
+                connectgaps= False, 
+                line=dict(color= colours_dict.get(i), width=2)),row=2, col=2)
+      fig.update_xaxes(title_text= 'Datetime', row=2, col=2)
+    
+    
+      
+  
+  
+  
+
+  
+  
+
+
+    
+
+
+
+    
+  fig.update_layout(template = "plotly_white", yaxis_title=y_axis,font=dict(size=15), height=900, margin=dict(l=50,r=50,b=50,t=50,pad=3))
+  # fig.update_layout(legend=dict(orientation="v",yanchor="top",y=0.95,xanchor="right",x=1.01))
+  fig.update_yaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
+  fig.update_xaxes(showline=True, linewidth=2, gridcolor='lightgray', linecolor='black')
+
+
+
+
+
+
+  return fig
+
+
+
+
+
+###############
+###############
+###############
 
 
 
@@ -60,19 +174,17 @@ flow_db = pd.read_sql('SELECT*FROM flowmeter', con=naturedb)
 
 #find unique reactors, then split their data into different dataframes
 reactor_names = flow_db['Name'].unique().tolist()
+reactor_names.sort()
 print(reactor_names)
 
 
-#create line thickness dictionary, then make AD bolder
-thick_dict = {reactor: 2 for reactor in reactor_names}
-thick_dict.update({'4A':5, '4B':5, '4C':5})
-# print(thick_dict)
+
 
 #create colours dictionary for plot lines
-colours_dict = {'1A': '#012969', '1B': '#2358ad', '1C': '#5f95ed', 
-                '2A': '#8a1d01', '2B': '#c94a2a', '2C': '#ed6a18',
-                '3A': '#297002', '3B': '#71d63a', '3C': '#64ed7d',
-                '4A': '#6e036e', '4B': '#ba25ba', '4C': '#e629b6'
+colours_dict = {'1A': '#F9C7CF', '1B': '#F6828C', '1C': '#F2404F', 
+                '2A': '#BFC7D9', '2B': '#8B9ABB', '2C': '#445374',
+                '3A': '#C7F2A7', '3B': '#9AE75F', '3C': '#5CB11B',
+                '4A': '#FF8533', '4B': '#FF6700', '4C': '#B84900'
 }
 
 print(flow_db)
@@ -88,8 +200,13 @@ print(current_db)
 
 
 
-df = current_db
-plot_plotly()
+
+fig_current = plot_plotly(current_db, 'Module Current (mA)', 'card_current_channel', False,reactor_names)
+fig_flow = plot_plotly(flow_db, 'total vol since start (ml)', 'total_vol_since_startml',True,reactor_names)
+
+
+fig_flow_subplot = plot_plotly_subplot(flow_db, 'total vol since start (ml)', 'total_vol_since_startml',reactor_names, 4)
+fig_current_subplot = plot_plotly_subplot(current_db, 'Module Current (mA)', 'card_current_channel',reactor_names, 3)
 
 
 
@@ -98,38 +215,51 @@ plot_plotly()
 
 
 
+# fig_current_G1 =  plot_plotly(current_db, 'Module Current', 'card_current_channel', False,reactor_names[0:3])
+# fig_current_G2 =  plot_plotly(current_db, 'Module Current', 'card_current_channel', False,reactor_names[3:6])
+# fig_current_G3 =  plot_plotly(current_db, 'Module Current', 'card_current_channel', False,reactor_names[6:9])
 
 
 
 
 
-#voltage dictionary to differentiate between different voltages
-#flow_dict = {reactor: flow_db.loc[flow_db['Name'] == reactor] for reactor in reactor_names}
-
-# # #create plots of each different reactor
-# plt.figure()
-# for reactor in flow_dict:
-#   sns.lineplot(x='index', y='total_vol_since_startml', data=flow_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
 
 
+# fig_current_G1.show()
+# fig_current_G2.show()
+# fig_current_G3.show()
 
-# #find unique reactors, then split their data into different dataframes
-# current_dict = {reactor: current_db.loc[current_db['Name'] == reactor] for reactor in reactor_names}
-# #print(current_dict)
 
-# #subsample each databse in curernt_dict as its quite big
-# for reactor in current_dict:
-#     current_dict[reactor] = current_dict[reactor].reset_index()
-#     current_dict[reactor] = current_dict[reactor][::10]
-#     current_dict[reactor]['card_current_channel'] = current_dict[reactor]['card_current_channel'].astype('float')
+app = dash.Dash()
+
+
+
+dcc.Interval 
+app.layout = html.Div(id = 'parent', children = [
+    html.H1(id = 'H1', children = 'Cabinet Reactor data', style = {'textAlign':'center','marginTop':40,'marginBottom':1}),
+       
+        dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'Current', 'value':fig_current},
+            {'label':'Flow', 'value':fig_flow},
+            {'label': 'Current Subplot', 'value':fig_current_subplot},
+            {'label': 'Flow Subplot', 'value':fig_flow_subplot},
+            ],
+        value = fig_flow_subplot),
+        dcc.Graph(id = 'bar_plot')
+    ])
     
+    
+@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
 
-# print('test reactor', current_dict['1A']['card_current_channel'])
-# #create plots of each different reactor
-# # plt.figure()
-# # for reactor in current_dict:
-# #   sns.lineplot(x='index', y='card_current_channel', data=current_dict[reactor].reset_index(), label=f'{reactor}', linewidth=thick_dict[reactor], color=colours_dict[reactor])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = dropdown_value
+    return fig  
 
 
-# # plt.ylim(-5, 30)
-# # plt.show()
+
+if __name__ == '__main__': 
+    app.run_server()
+
