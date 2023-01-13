1613a93bb28edfa720a9972bb1d53a1e39b06321
harveyrutland <hcrutland@mail.com>
2023-01-13 17:10:00 +0000

    rs code update

--- rohde_schwarz/pushtosql_rs.py
+++ rohde_schwarz/pushtosql_rs.py
@@ -131,7 +131,7 @@ power_df = pd.concat(df_ls, axis=0)
 
 print(power_df)
 power_df.reset_index(inplace=True)
-power_df = power_df.rename(columns={"V": "voltageV", "A": "currentA", "P", "powerP", 'ID':'Name'})
+power_df = power_df.rename(columns={"V": "voltageV", "A": "currentA", "P": "powerP", 'ID':'Name'})
 
 
 
