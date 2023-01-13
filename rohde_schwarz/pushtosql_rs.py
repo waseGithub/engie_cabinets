249722efe7aa005fdf50819f427c6e95d282fca5
harveyrutland <hcrutland@mail.com>
2023-01-13 17:22:15 +0000

    rs code update

--- rohde_schwarz/pushtosql_rs.py
+++ rohde_schwarz/pushtosql_rs.py
@@ -88,8 +88,8 @@ def append_rs_csv_to_ls(df, tank_names_ls):
     concat_df.set_index(['datetime', 'ID'], inplace=True)
     concat_df = concat_df.groupby([pd.Grouper(freq='5T', level='datetime'), pd.Grouper(level='ID')])['V', 'A', 'P'].mean()  
 
-    if df == df_bot | df == df_mid: 
-        df = df.shift(periods=-2)
+    # if df == df_bot | df == df_mid: 
+    #     df = df.shift(periods=-2)
 
     return concat_df
     
